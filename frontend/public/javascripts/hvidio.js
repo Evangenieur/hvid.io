a055f1ae19aa3f432cc7538702ce5b630a520805
Jay Salvat <jay@jaysalvat.com>
2012-12-01 15:57:20 +0100

    Ajoute le loading

--- frontend/public/javascripts/hvidio.js
+++ frontend/public/javascripts/hvidio.js
@@ -1,3 +1,26 @@
-hvd = {
-    
-}
+var hvidio;
+
+(function() {
+    var loader;
+
+    hvidio = {
+        init: function() {
+            loader = new CanvasLoader('loading');
+            loader.setColor('#99CC32');
+            loader.setDiameter(32);
+            loader.setDensity(32);
+            loader.setRange(0.6);
+            loader.setSpeed(1);
+        },
+        loading: function(bool) {
+            if (bool) {
+                loader.show();
+            } else {
+                loader.hide();
+            }
+        }
+    }
+})();
+
+hvidio.init();
+hvidio.loading(true);
